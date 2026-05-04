273f7126a5c9ba9d6c49518d0defba3e8a4eb8fb
Audrey COMLAN-CATARIA <audreycomlancataria@gmail.com>
2026-05-04 03:29:49 +0200

    fix: replace BrowserRouter with HashRouter for routing

--- src/main.tsx
+++ src/main.tsx
@@ -1,17 +1,17 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
-import { BrowserRouter, Route, Routes } from 'react-router'
-import DrinksMenu from './DrinksMenu'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.tsx";
+import { HashRouter, Route, Routes } from "react-router";
+import DrinksMenu from "./DrinksMenu";
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<App />} />
-      <Route path="/bar-lounge/drinks-menu" element={<DrinksMenu />} />
-    </Routes>
-  </BrowserRouter>
+    <HashRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/bar-lounge/drinks-menu" element={<DrinksMenu />} />
+      </Routes>
+    </HashRouter>
   </StrictMode>
-)
+);
