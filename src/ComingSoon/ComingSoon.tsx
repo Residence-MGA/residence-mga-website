305fefb69c36388f227ea85c18f136deef7bd52b
Audrey COMLAN-CATARIA <audreycomlancataria@gmail.com>
2026-08-17 01:05:32 +0200

    fat: add contact info

--- src/ComingSoon/ComingSoon.tsx
+++ src/ComingSoon/ComingSoon.tsx
@@ -1,9 +1,15 @@
 import mgaLogo from "../assets/MGA_logo.png";
 import {
   ComingSoonContainerStyles,
+  ContactDetailStyles,
+  ContactDetailsStyles,
+  ContactSectionStyles,
   DividerStyles,
   FooterTextStyles,
+  LocationStyles,
   LogoStyles,
+  SocialLinkStyles,
+  SocialLinksStyles,
   SubtitleStyles,
 } from "./ComingSoon.styled";
 
@@ -21,6 +27,42 @@ const ComingSoon = () => {
       <FooterTextStyles>
         L'excellence se prépare. Suivez-nous pour l'ouverture du site.
       </FooterTextStyles>
+
+      <ContactSectionStyles>
+        <LocationStyles>
+          📍 Gbèna Sud, Ouidah - Bénin
+        </LocationStyles>
+
+        <ContactDetailsStyles>
+          <ContactDetailStyles>
+            📧{" "}
+            <a href="mailto:mgaresidence@gmail.com">
+              mgaresidence@gmail.com
+            </a>
+          </ContactDetailStyles>
+
+          <ContactDetailStyles>
+            📱 +229 (01) 62 31 35 65 • (01) 51 28 02 81
+          </ContactDetailStyles>
+        </ContactDetailsStyles>
+
+        <SocialLinksStyles>
+          <SocialLinkStyles
+            href="https://www.instagram.com/residence_mga"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Instagram
+          </SocialLinkStyles>
+          <SocialLinkStyles
+            href="https://www.facebook.com/people/Residence-Mga/61589306677535"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Facebook
+          </SocialLinkStyles>
+        </SocialLinksStyles>
+      </ContactSectionStyles>
     </ComingSoonContainerStyles>
   );
 };
